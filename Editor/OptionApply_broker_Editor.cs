34a410d0525a7a835548bd2da7ec8f71de6fdda6
dimaTidev <timofeenkodima@gmail.com>
2021-10-29 16:14:42 +0200

    Changes

---
+++ Editor/OptionApply_broker_Editor.cs
@@ -0,0 +1,86 @@
+﻿using OptionsConstructor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(OptionApply_broker))]
+[CanEditMultipleObjects]
+public class OptionApply_brokerEditor : Editor
+{
+     SerializedProperty option;
+
+    string[] eventsNames =
+    {
+        "onChange_int",
+        "onChange_bool",
+        "onChange_float",
+        "onChange_string"
+    };
+
+    int idSelected = -1;
+    SerializedProperty[] events;
+
+    void OnEnable()
+    {
+       
+        option = serializedObject.FindProperty("option");
+
+        events = new SerializedProperty[eventsNames.Length];
+        for (int i = 0; i < events.Length; i++)
+            events[i] = serializedObject.FindProperty(eventsNames[i]);
+
+        Refresh(option);
+    }
+
+    public override void OnInspectorGUI()
+    {
+       // DrawDefaultInspector();
+
+        serializedObject.Update();
+
+        EditorGUI.BeginChangeCheck();
+        EditorGUILayout.PropertyField(option); // new GUIContent("Vector Object")
+        if (EditorGUI.EndChangeCheck())
+            Refresh(option);
+        
+
+        for (int i = 0; i < events.Length; i++)
+        {
+            GUI.color = idSelected == i || idSelected == -1 ? Color.white : Color.grey;
+            GUI.enabled = idSelected == i || idSelected == -1;
+            EditorGUILayout.PropertyField(events[i]);
+        }
+        GUI.enabled = true;
+        GUI.color = Color.white;
+        //
+        // EditorGUILayout.PropertyField(damageProp, new GUIContent("Property"));
+        //
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    void Refresh(SerializedProperty option)
+    {
+        if (option.objectReferenceValue is AOptionsScriptable opt)
+        {
+            Type returnType = opt.Get_GenValue().GetType();
+
+            if (returnType == typeof(int))
+            {
+                if (opt is AToggle)
+                    idSelected = 0;
+                else
+                    idSelected = 1;
+            }
+            else if (returnType == typeof(float))
+                idSelected = 2;
+            else if (returnType == typeof(string))
+                idSelected = 3;
+        }
+        else
+            idSelected = -1;
+    }
+}
--- Runtime/OptionsScriptable/Brokers/OptionApply_broker.cs
+++ Runtime/OptionsScriptable/Brokers/OptionApply_broker.cs
@@ -9,10 +9,12 @@ namespace OptionsConstructor
     public class OptionApply_broker : AOptionApply
     {
         [SerializeField] UnityEvent_int onChange_int = null;
+        [SerializeField] UnityEvent_bool onChange_bool = null;
         [SerializeField] UnityEvent_float onChange_float = null;
         [SerializeField] UnityEvent_string onChange_string = null;
 
         [System.Serializable] public class UnityEvent_int : UnityEvent<int>{ }
+        [System.Serializable] public class UnityEvent_bool : UnityEvent<bool>{ }
         [System.Serializable] public class UnityEvent_float : UnityEvent<float>{ }
         [System.Serializable] public class UnityEvent_string : UnityEvent<string>{ }
 
@@ -25,7 +27,10 @@ namespace OptionsConstructor
             IConvertible value = option.Get_GenValue();
             
             if (value.GetType() == typeof(int))
+            {
                 onChange_int?.Invoke((int)value);
+                onChange_bool?.Invoke((int)value != 0);
+            }
             else if (value.GetType() == typeof(float))
                 onChange_float?.Invoke((float)value);
             else if (value.GetType() == typeof(string))
@@ -45,6 +50,7 @@ namespace OptionsConstructor
         #region Test
         //------------------------------------------------------------------------------------------------------------
         public void TEST_SetValueInt(int value) => Debug.Log("Set int value: " + value);
+        public void TEST_SetValueBool(bool value) => Debug.Log("Set bool value: " + value);
         public void TEST_SetValueFloat(float value) => Debug.Log("Set float value: " + value);
         public void TEST_SetValueString(string value) => Debug.Log("Set string value: " + value);
         //------------------------------------------------------------------------------------------------------------
